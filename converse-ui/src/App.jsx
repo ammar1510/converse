ffa675d0bacecb361f9b9857862372f9796a7452
Ammar3 Shaikh <ammarshaikh1510@gmail.com>
2025-03-22 03:01:29 +0530

    chat takes up full page

--- converse-ui/src/App.jsx
+++ converse-ui/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { ChatProvider } from './context/ChatContext';
 import ProtectedRoute from './routes/ProtectedRoute';
@@ -9,6 +9,36 @@ import Navbar from './components/layout/Navbar';
 import './App.css';
 
 /**
+ * Layout component that conditionally renders Navbar
+ */
+function AppLayout() {
+  const location = useLocation();
+  const isChatPage = location.pathname === '/chat';
+  
+  return (
+    <div className="app-container">
+      <Navbar />
+      <main className={`main-content ${isChatPage ? 'full-height' : ''}`}>
+        <Routes>
+          {/* Public routes */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          
+          {/* Protected routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/chat" element={<ChatPage />} />
+          </Route>
+          
+          {/* Redirects */}
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+/**
  * Main App component
  * Sets up routing and authentication context
  */
@@ -17,25 +47,7 @@ function App() {
     <AuthProvider>
       <ChatProvider>
         <BrowserRouter>
-          <div className="app-container">
-            <Navbar />
-            <main className="main-content">
-              <Routes>
-                {/* Public routes */}
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                
-                {/* Protected routes */}
-                <Route element={<ProtectedRoute />}>
-                  <Route path="/chat" element={<ChatPage />} />
-                </Route>
-                
-                {/* Redirects */}
-                <Route path="/" element={<Navigate to="/login" replace />} />
-                <Route path="*" element={<Navigate to="/login" replace />} />
-              </Routes>
-            </main>
-          </div>
+          <AppLayout />
         </BrowserRouter>
       </ChatProvider>
     </AuthProvider>
